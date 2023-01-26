c458af0dc60bb2bde899cfd35fde8b28238bc816
Bipperty <ignacio.gibbs@gmail.com>
2023-01-26 16:42:24 -0300

    App & DB config

---
+++ README.md
@@ -0,0 +1,29 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# E-Shopping API
+
+1. Clonar el repositorio
+
+2. Instalar las dependencias
+
+```bash
+npm install
+```
+
+3. Clonar el archivo ___env.template__ y renombrarlo a __.env__
+
+4. Cambiar las variables de entorno
+
+5. Levantar la base de datos
+
+```bash
+docker-compose up -d
+```
+
+6. Levantar el servidor en desarrollo
+
+```bash
+npm run start:dev
+```
---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module, ParseBoolPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, //Poner en false en producción
+    }),
+  ],
+})
+export class AppModule {}
